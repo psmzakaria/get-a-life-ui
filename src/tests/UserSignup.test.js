4dad035fcb57a1ce8347f21e2defe391ed391d77
Derek <derek.chan522@gmail.com>
2018-08-01 22:47:43 +0800

    fix Home and UserSignup component tests

--- src/tests/UserSignup.test.js
+++ src/tests/UserSignup.test.js
@@ -4,8 +4,16 @@ import UserSignup from "./../components/UserSignup";
 import ShallowRenderer from "react-test-renderer/shallow";
 
 it("renders the sign up page ", () => {
+  const state = {
+    hideError: true,
+    errorHeader: "",
+    errorContent: "",
+    username: "",
+    password: ""
+  };
+
   const renderer = new ShallowRenderer();
-  renderer.render(<UserSignup />);
+  renderer.render(<UserSignup state={state} />);
   const output = renderer.getRenderOutput();
   expect(output).toMatchSnapshot();
 });
