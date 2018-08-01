7d9dff11d405e2d3084d14fbf08f064fe9c40d78
qwetuo <chewminzhuang@hotmail.com>
2018-08-01 23:15:25 +0800

    updated profile bar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,20 +1,31 @@
 import React, { Component } from "react";
-import { Grid } from "semantic-ui-react";
+import { Grid, Icon } from "semantic-ui-react";
 import EOrganiser from "./EOrganiser";
+
+const mockUserData = {
+  username: "user1"
+};
 
 class Home extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      username: mockUserData.username
+    };
   }
   render() {
     return (
       <div>
         <Grid columns={2} divided>
-          <Grid.Column width={4} color="violet">
-            <p>username</p>
+          <Grid.Column textAlign="center" width={4} color="violet" >
+              <h2>Profile</h2>
+              <Icon size="huge" inverted color="teal" circular name="user" />
+              <h3>
+                <br />
+                {this.state.username}
+              </h3>
           </Grid.Column>
-          <Grid.Column width={9}>
+          <Grid.Column textAlign="center" width={9}>
             <EOrganiser />
           </Grid.Column>
         </Grid>
