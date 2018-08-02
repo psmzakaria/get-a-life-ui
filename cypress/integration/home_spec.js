cdd9fc340a3ecc37073acf3d1a0c80fd3de409e2
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-08-02 16:04:36 +0800

    Cypress tests for signup and signin

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,20 +1,36 @@
 /// <reference types="Cypress" />
-
+const URL = 'http://localhost:3001/';
 context('Homepage test', () => {
 	beforeEach(() => {
-		cy.visit('http://localhost:3001');
+		cy.visit(URL);
 	});
 
-	it('type username/pw into username/pw input', () => {
-		cy.get('#username').type('user01').should('have.value', 'user01');
+	it('type username/pw into username/pw input of signup', () => {
+		cy.get('#username').clear().type('user01').should('have.value', 'user01');
 		cy.get('#password').type('password01').should('have.value', 'password01');
+		cy.get('#signup').click();
+		cy.url().should('eq', URL + 'events');
 	});
 
-	it('should submit username and pw for signup and redirect', () => {
+	it('should submit username and pw for signin and redirect', () => {
 		cy.get('.cursor-pointer').click();
 		cy.get('#username').type('user01').should('have.value', 'user01');
 		cy.get('#password').type('password01').should('have.value', 'password01');
 		cy.get('#signin').click();
-		cy.url().should('eq', 'http://localhost:3001/events');
+		cy.url().should('eq', URL + 'events');
+	});
+
+	it('should remain in homepage for duplicate user sign up', () => {
+		cy.get('#username').clear().type('user01');
+		cy.get('#password').type('password01');
+		cy.get('#signup').click();
+		cy.url().should('eq', URL);
+	});
+
+	it('should remain in homepage for blank username sign up', () => {
+		cy.get('#username').clear().type(' ');
+		cy.get('#password').type('password01');
+		cy.get('#signup').click();
+		cy.url().should('eq', URL);
 	});
 });
