0f92b094fdbc4ea42509372cba238056bf05e4b9
Derek <derek.chan522@gmail.com>
2018-08-03 11:42:09 +0800

    modify cypress test to check for username after login

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,40 +1,54 @@
 /// <reference types="Cypress" />
-const URL = 'http://localhost:3001/';
+const URL = "http://localhost:3001/";
 
-context('Homepage test', () => {
-	before(() => {
-		cy.exec('npm run db:reset');
-	});
-	beforeEach(() => {
-		cy.visit(URL);
-	});
+context("Homepage test", () => {
+  before(() => {
+    cy.exec("npm run db:reset");
+  });
+  beforeEach(() => {
+    cy.visit(URL);
+  });
 
-	it('type username/pw into username/pw input of signup', () => {
-		cy.get('#username').clear().type('user01').should('have.value', 'user01');
-		cy.get('#password').type('password01').should('have.value', 'password01');
-		cy.get('#signup').click();
-		cy.url().should('eq', URL + 'user01/events');
-	});
+  it("type username/pw into username/pw input of signup", () => {
+    cy.get("#username")
+      .clear()
+      .type("user01")
+      .should("have.value", "user01");
+    cy.get("#password")
+      .type("password01")
+      .should("have.value", "password01");
+    cy.get("#signup").click();
+    cy.url().should("eq", URL + "user01/events");
+  });
 
-	it('should submit username and pw for signin and redirect', () => {
-		cy.get('.cursor-pointer').click();
-		cy.get('#username').type('user01').should('have.value', 'user01');
-		cy.get('#password').type('password01').should('have.value', 'password01');
-		cy.get('#signin').click();
-		cy.url().should('eq', URL + 'user01/events');
-	});
+  it("should submit username and pw for signin and redirect", () => {
+    cy.get(".cursor-pointer").click();
+    cy.get("#username")
+      .type("user01")
+      .should("have.value", "user01");
+    cy.get("#password")
+      .type("password01")
+      .should("have.value", "password01");
+    cy.get("#signin").click();
+    cy.url().should("eq", URL + "user01/events");
+    cy.get("#username-display").contains("user01");
+  });
 
-	it('should remain in homepage for duplicate user sign up', () => {
-		cy.get('#username').clear().type('user01');
-		cy.get('#password').type('password01');
-		cy.get('#signup').click();
-		cy.url().should('eq', URL);
-	});
+  it("should remain in homepage for duplicate user sign up", () => {
+    cy.get("#username")
+      .clear()
+      .type("user01");
+    cy.get("#password").type("password01");
+    cy.get("#signup").click();
+    cy.url().should("eq", URL);
+  });
 
-	it('should remain in homepage for blank username sign up', () => {
-		cy.get('#username').clear().type(' ');
-		cy.get('#password').type('password01');
-		cy.get('#signup').click();
-		cy.url().should('eq', URL);
-	});
+  it("should remain in homepage for blank username sign up", () => {
+    cy.get("#username")
+      .clear()
+      .type(" ");
+    cy.get("#password").type("password01");
+    cy.get("#signup").click();
+    cy.url().should("eq", URL);
+  });
 });
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,59 +1,58 @@
-import React, { Component } from 'react';
-import { API_URL } from '../utils/configVar';
-import { Grid, Icon } from 'semantic-ui-react';
-import EOrganiser from './EOrganiser';
+import React, { Component } from "react";
+import { API_URL } from "../utils/configVar";
+import { Grid, Icon } from "semantic-ui-react";
+import EOrganiser from "./EOrganiser";
 
 class Home extends Component {
-	constructor() {
-		super();
-		this.state = {
-			username: ''
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      username: ""
+    };
+  }
 
-	getUserData = async () => {
-		const username = this.props.location.pathname.split('/')[1];
-		const response = await fetch(`${API_URL}/users/${username}`, {
-			method: 'GET',
-			credentials: 'include',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json'
-			}
-		});
+  getUserData = async () => {
+    const username = this.props.location.pathname.split("/")[1];
+    const response = await fetch(`${API_URL}/users/${username}`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    });
 
-		if (response.ok) {
-			const userData = await response.json();
-			this.setState({
-				username: userData.username
-			});
-		}
-	};
-	componentDidMount = () => {
-		this.getUserData();
-	};
-	render() {
-		console.log(this.state.username);
-		return (
-			<div>
-				<Grid columns={2} divided>
-					<Grid.Column textAlign="center" width={4} color="violet">
-						<div className="profile-container">
-							<h2>Profile</h2>
-							<Icon size="huge" inverted color="teal" circular name="user" />
-							<h3>
-								<br />
-								{this.state.username}
-							</h3>
-						</div>
-					</Grid.Column>
-					<Grid.Column width={9}>
-						<EOrganiser />
-					</Grid.Column>
-				</Grid>
-			</div>
-		);
-	}
+    if (response.ok) {
+      const userData = await response.json();
+      this.setState({
+        username: userData.username
+      });
+    }
+  };
+
+  componentDidMount = () => {
+    this.getUserData();
+  };
+
+  render() {
+    console.log(this.state.username);
+    return (
+      <div>
+        <Grid columns={2} divided>
+          <Grid.Column textAlign="center" width={4} color="violet">
+            <div className="profile-container">
+              <h2>Profile</h2>
+              <Icon size="huge" inverted color="teal" circular name="user" />
+              <h3 id="username-display">{this.state.username}</h3>
+            </div>
+          </Grid.Column>
+          <Grid.Column width={9}>
+            <EOrganiser />
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Home;
