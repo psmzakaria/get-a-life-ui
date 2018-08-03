ac1460219ea1cffd4ead8fc9e70e74a69dcc9dc8
psmzakaria <psmzakaria@gmail.com>
2018-08-03 10:28:38 +0800

    Added event specs

--- cypress/integration/event_spec.js
+++ cypress/integration/event_spec.js
@@ -11,3 +11,18 @@ context("Homepage test", () => {
 });
 
 
+it("type username/pw into username/pw input of signup", () => {
+    cy.get(".ui-card")
+      .clear()
+      .type("My New Event")
+      .should("have.value"  , "My New Event");
+    cy.get("#")
+      .type("30/08/2018")
+      .should("have.value", "30/08/2018");
+      cy.get("#")
+      .type("15/09/2018")
+      .should("have.value", "15/09/2018");
+
+    cy.get("#signup").click();
+    cy.url().should("eq", URL + "user01/events");
+  });
