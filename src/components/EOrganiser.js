d641b1edb84c7acf895e507b943abcdf51be9f83
Derek <derek.chan522@gmail.com>
2018-08-07 16:47:17 +0800

    refactor EOrganiser.js

--- src/components/EOrganiser.js
+++ src/components/EOrganiser.js
@@ -1,45 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, Divider } from "semantic-ui-react";
 import ESnap from "./ESnap";
 
-class EOrganiser extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+const getESnap = (event, status) => {
+  return (
+    <ESnap
+      key={event._id}
+      title={event.title}
+      host={event.hostId.username}
+      status={status}
+    />
+  );
+};
 
-  getESnap = (event, status) => {
-    return (
-      <ESnap
-        key={event._id}
-        title={event.title}
-        host={event.hostId.username}
-        status={status}
-      />
-    );
-  };
-
-  render() {
-    return (
-      <div className="eorganiser-container">
-        <h2> Your Hosted Events</h2>
-        <Card.Group centered>
-          {this.props.hostedEvents.map((event, index) => {
-            return this.getESnap(event, this.props.statuses[index]);
-          })}
-        </Card.Group>
-        <Divider />
-        <h2> Your Invited Events</h2>
-        <Card.Group centered>
-          {this.props.invitedEvents.map((event, index) => {
-            return this.getESnap(event, this.props.statuses[index]);
-          })}
-        </Card.Group>
-        <Divider />
-        <h2> You have a life</h2>
-      </div>
-    );
-  }
-}
+const EOrganiser = props => {
+  return (
+    <div className="eorganiser-container">
+      <h2> Your Hosted Events</h2>
+      <Card.Group centered>
+        {props.hostedEvents.map((event, index) => {
+          return getESnap(event, props.statuses[index]);
+        })}
+      </Card.Group>
+      <Divider />
+      <h2> Your Invited Events</h2>
+      <Card.Group centered>
+        {props.invitedEvents.map((event, index) => {
+          return getESnap(event, props.statuses[index]);
+        })}
+      </Card.Group>
+      <Divider />
+      <h2> You have a life</h2>
+    </div>
+  );
+};
 
 export default EOrganiser;
