e389940b0897da431207bccdd3fd6673b1536eaf
Derek <derek.chan522@gmail.com>
2018-08-07 16:54:02 +0800

    refactored snap

--- src/components/ESnap.js
+++ src/components/ESnap.js
@@ -1,34 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card } from "semantic-ui-react";
 
-class ESnap extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className="esnap-container-size">
-        <Card color="red">
-          {/* <div className="esnap-icon">
+const ESnap = props => {
+  return (
+    <div className="esnap-container-size">
+      <Card color="red">
+        {/* <div className="esnap-icon">
             <Icon name="calendar alternate outline" size="massive" />
           </div> */}
-          <Card.Content>
-            <Card.Header>{this.props.title}</Card.Header>
-            <Card.Meta>
-              <span>Host: {this.props.host}</span>
-            </Card.Meta>
-            <Card.Description>KIV - information/details</Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              {this.props.status}
-            </a>
-          </Card.Content>
-        </Card>
-      </div>
-    );
-  }
-}
+        <Card.Content>
+          <Card.Header>{props.title}</Card.Header>
+          <Card.Meta>
+            <span>Host: {props.host}</span>
+          </Card.Meta>
+          <Card.Description>KIV - information/details</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a>{props.status}</a>
+        </Card.Content>
+      </Card>
+    </div>
+  );
+};
 
 export default ESnap;
