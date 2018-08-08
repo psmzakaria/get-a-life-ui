4a048960b58048381908783858a12c66d65c6f71
Derek <derek.chan522@gmail.com>
2018-08-08 14:42:34 +0800

    show no events messages in dashboard

--- src/components/EOrganiser.js
+++ src/components/EOrganiser.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Divider } from "semantic-ui-react";
+import { Card, Divider, Header } from "semantic-ui-react";
 import ESnap from "./ESnap";
 
 const getESnap = (event, status) => {
@@ -10,20 +10,37 @@ const EOrganiser = props => {
   return (
     <div className="eorganiser-container">
       <h2> Your Hosted Events</h2>
-      <Card.Group>
-        {props.hostedEvents.map((event, index) => {
-          return getESnap(event, props.statuses[index]);
-        })}
-      </Card.Group>
+      {props.hostedEvents.length > 0 && (
+        <Card.Group>
+          {props.hostedEvents.map((event, index) => {
+            return getESnap(event, props.statuses[index]);
+          })}
+        </Card.Group>
+      )}
+      {props.hostedEvents.lenght === 0 && (
+        <Header as="h2" textAlign="center" disabled>
+          You have no hosted events
+        </Header>
+      )}
       <Divider />
       <h2> Your Invited Events</h2>
-      <Card.Group>
-        {props.invitedEvents.map((event, index) => {
-          return getESnap(event, props.statuses[index]);
-        })}
-      </Card.Group>
+      {props.invitedEvents.length > 0 && (
+        <Card.Group>
+          {props.invitedEvents.map((event, index) => {
+            return getESnap(event, props.statuses[index]);
+          })}
+        </Card.Group>
+      )}
+      {props.invitedEvents.length === 0 && (
+        <Header as="h2" textAlign="center" disabled>
+          You have not been invited
+        </Header>
+      )}
       <Divider />
       <h2> You have a life</h2>
+      <Header as="h2" textAlign="center" disabled>
+        Soon...
+      </Header>
     </div>
   );
 };
