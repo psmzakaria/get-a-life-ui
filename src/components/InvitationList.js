bd4e537312dad151a99f2455b81a9d6ffb2fe2ed
qwetuo <chewminzhuang@hotmail.com>
2018-08-10 17:47:17 +0800

    codesmell - amend indentation

--- src/components/InvitationList.js
+++ src/components/InvitationList.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import { Button, List, Segment } from 'semantic-ui-react';
+import React from "react";
+import { Button, List, Segment } from "semantic-ui-react";
 
-const InvitationList = (props) => {
-	return (
-		<div>
-			<h2>Invitation List</h2>
-			<Segment padded="very">
-				<List divided verticalAlign="middle">
-					{props.invitees.map((invitee, index) => (
-						<List.Item key={index}>
-							<List.Content floated="right">
-								<Button
-									color="red"
-									size="tiny"
-									onClick={(event) => props.handleDeleteFromList(event, invitee)}
-								>
-									-
-								</Button>
-							</List.Content>
-							<List.Content>{invitee}</List.Content>
-						</List.Item>
-					))}
-				</List>
-			</Segment>
-		</div>
-	);
+const InvitationList = props => {
+  return (
+    <div>
+      <h2>Invitation List</h2>
+      <Segment padded="very">
+        <List divided verticalAlign="middle">
+          {props.invitees.map((invitee, index) => (
+            <List.Item key={index}>
+              <List.Content floated="right">
+                <Button
+                  color="red"
+                  size="tiny"
+                  onClick={event => props.handleDeleteFromList(event, invitee)}
+                >
+                  -
+                </Button>
+              </List.Content>
+              <List.Content>{invitee}</List.Content>
+            </List.Item>
+          ))}
+        </List>
+      </Segment>
+    </div>
+  );
 };
 
 export default InvitationList;
--- src/components/SearchUsernameBar.js
+++ src/components/SearchUsernameBar.js
@@ -1,22 +1,24 @@
-import _ from 'lodash';
-import React from 'react';
-import { Search, Grid } from 'semantic-ui-react';
+import _ from "lodash";
+import React from "react";
+import { Search, Grid } from "semantic-ui-react";
 
-const SearchUsernameBar = (props) => {
-	return (
-		<Grid>
-			<Grid.Column>
-				<Search
-					loading={props.isLoading}
-					onResultSelect={props.handleResultSelect}
-					onSearchChange={_.debounce(props.handleSearchChange, 500, { leading: true })}
-					results={props.results}
-					value={props.value}
-					fluid={true}
-				/>
-			</Grid.Column>
-		</Grid>
-	);
+const SearchUsernameBar = props => {
+  return (
+    <Grid>
+      <Grid.Column>
+        <Search
+          loading={props.isLoading}
+          onResultSelect={props.handleResultSelect}
+          onSearchChange={_.debounce(props.handleSearchChange, 500, {
+            leading: true
+          })}
+          results={props.results}
+          value={props.value}
+          fluid={true}
+        />
+      </Grid.Column>
+    </Grid>
+  );
 };
 
 export default SearchUsernameBar;
