fa60dd2ba2d4601465cd6948d06b27893c517b97
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-08-15 11:59:44 +0800

    Fix failing cypress tests and add script to run cypress-headless-ci on circleci

--- cypress/integration/event_spec.js
+++ cypress/integration/event_spec.js
@@ -1,67 +1,87 @@
 /// <reference types="Cypress" />
-const URL = 'http://localhost:3001/';
+const { URL, userGuest, userHost } = require("../fixtures/testData");
 
-const signupUser = (username) => {
-	cy.visit(URL);
-	cy.get('#username').clear().type(username);
-	cy.get('#password').type('password01');
-	cy.get('#signup').click();
+const signupUser = username => {
+  cy.visit(URL);
+  cy.get("#username")
+    .clear()
+    .type(username);
+  cy.get("#password").type("password01");
+  cy.get("#signup").click();
 };
 
-const signinUser = (username) => {
-	cy.visit(URL);
-	cy.get('a').contains('Sign In Here').click();
-	cy.get('#username').clear().type(username);
-	cy.get('#password').type('password01');
-	cy.get('#signin').click();
+const signinUser = username => {
+  cy.visit(URL);
+  cy.get("a")
+    .contains("Sign In Here")
+    .click();
+  cy.get("#username")
+    .clear()
+    .type(username);
+  cy.get("#password").type("password01");
+  cy.get("#signin").click();
 };
 
 const createEvent = () => {
-	cy.get('.calendar').click();
-	cy.get('#title').clear().type('My New Event').should('have.value', 'My New Event');
-	cy.get('.Cal__Day__today').click();
-	cy.get('.Cal__Day__today + li').click();
-	cy.get('.ui.button').contains('Invite Friends!').click();
+  cy.get(".calendar").click();
+  cy.get("#title")
+    .clear()
+    .type("My New Event")
+    .should("have.value", "My New Event");
+  cy.get(".Cal__Day__today").click();
+  cy.get(".Cal__Day__today + li").click();
+  cy.get(".ui.button")
+    .contains("Invite Friends!")
+    .click();
 };
 
-const inviteUser = (username) => {
-	cy.get('.prompt').type(username);
-	cy.get('.content .title').click();
-	cy.get('.actions').find('.button').contains('All Done').click();
+const inviteUser = username => {
+  cy.get(".prompt").type(username);
+  cy.get(".content .title").click();
+  cy.get(".actions")
+    .find(".button")
+    .contains("All Done")
+    .click();
 };
 
 const removeUser = () => {
-	cy.get('.item').find('.button').contains('-').click();
+  cy.get(".item")
+    .find(".button")
+    .contains("-")
+    .click();
 };
 
-context('Eventpage test', () => {
-	beforeEach(() => {
-		cy.exec('npm run db:reset');
-		signupUser('userGuest');
-		signupUser('userHost');
-	});
+context("Eventpage test", () => {
+  beforeEach(() => {
+    signupUser(userGuest);
+    signupUser(userHost);
+  });
 
-	it('create an event then add friends', () => {
-		createEvent();
-		inviteUser('userGuest');
+  it("create an event then add friends", () => {
+    createEvent();
+    inviteUser(userGuest);
 
-		cy.get('.esnap-container-size');
-	});
+    cy.get(".esnap-container-size");
+  });
 
-	it.only('as a guest, i should see the event which i am invited to', () => {
-		createEvent();
-		inviteUser('userGuest');
+  it.only("as a guest, i should see the event which i am invited to", () => {
+    createEvent();
+    inviteUser(userGuest);
 
-		signinUser('userGuest');
+    signinUser(userGuest);
 
-		cy.get('h2').contains('Your Invited Events').next().get('.esnap-container-size').should('have.length', 1);
-	});
+    cy.get("h2")
+      .contains("Your Invited Events")
+      .next()
+      .get(".esnap-container-size")
+      .should("have.length", 1);
+  });
 
-	it('create an event, add friend then remove', () => {
-		createEvent();
-		inviteUser('userGuest');
+  it("create an event, add friend then remove", () => {
+    createEvent();
+    inviteUser(userGuest);
 
-		removeUser();
-		cy.get('.list').should('be.empty');
-	});
+    removeUser();
+    cy.get(".list").should("be.empty");
+  });
 });
--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,45 +1,51 @@
 /// <reference types="Cypress" />
-const URL = 'http://localhost:3001/';
 
-context('Homepage test', () => {
-	before(() => {
-		cy.exec('npm run db:reset');
-	});
-	beforeEach(() => {
-		cy.visit(URL);
-	});
+const { URL, user01 } = require("../fixtures/testData");
 
-	it('should return successful signup of user01', () => {
-		cy.get('#username').clear().type('user01').should('have.value', 'user01');
-		cy.get('#password').type('password01').should('have.value', 'password01');
-		cy.get('#signup').click();
-		cy.url().should('eq', URL + 'user01/events');
-	});
+context("Homepage test", () => {
+  beforeEach(() => {
+    cy.visit(URL);
+  });
 
-	it('should return successful signin of user01 and redirect to /user01/events', () => {
-		cy.get('.cursor-pointer').click();
-		cy.get('#username').type('user01').should('have.value', 'user01');
-		cy.get('#password').type('password01').should('have.value', 'password01');
-		cy.get('#signin').click();
-		cy.url().should('eq', URL + 'user01/events');
-		cy.get('#username-display').contains('user01');
-	});
+  it("should return successful signup of user01", () => {
+    cy.get("#username")
+      .clear()
+      .type(user01);
+    cy.get("#password").type("password01");
+    cy.get("#signup").click();
+    cy.url().should("eq", URL + `${user01}/events`);
+  });
 
-	it.skip('should remain in homepage for duplicate user sign up w/ error message', () => {
-		cy.get('#username').clear().type('user01');
-		cy.get('#password').type('password01');
-		cy.get('#signup').click();
-		cy.get('.ui.negative.message').contains('User validation failed: username: Username already exists');
-		cy.url().should('eq', URL);
-	});
+  it("should return successful signin of user and redirect to :username/events", () => {
+    cy.get(".cursor-pointer").click();
+    cy.get("#username").type(user01);
+    cy.get("#password").type("password01");
+    cy.get("#signin").click();
+    cy.url().should("eq", URL + `${user01}/events`);
+    cy.get("#username-display").contains(user01);
+  });
 
-	it.skip('should remain in homepage for blank username sign up w/ error message', () => {
-		cy.get('#username').clear().type(' ');
-		cy.get('#password').type('password01');
-		cy.get('#signup').click();
-		cy
-			.get('.ui.negative.message')
-			.contains('User validation failed: username: is invalid, username contains only alphanumeric characters');
-		cy.url().should('eq', URL);
-	});
+  it.skip("should remain in homepage for duplicate user sign up w/ error message", () => {
+    cy.get("#username")
+      .clear()
+      .type(user01);
+    cy.get("#password").type("password01");
+    cy.get("#signup").click();
+    cy.get(".ui.negative.message").contains(
+      "User validation failed: username: Username already exists"
+    );
+    cy.url().should("eq", URL);
+  });
+
+  it.skip("should remain in homepage for blank username sign up w/ error message", () => {
+    cy.get("#username")
+      .clear()
+      .type(" ");
+    cy.get("#password").type("password01");
+    cy.get("#signup").click();
+    cy.get(".ui.negative.message").contains(
+      "User validation failed: username: is invalid, username contains only alphanumeric characters"
+    );
+    cy.url().should("eq", URL);
+  });
 });
