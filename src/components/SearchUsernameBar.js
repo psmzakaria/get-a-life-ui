1d4937d1967551746c33f8b411e222c13729de43
Derek <derek.chan522@gmail.com>
2018-08-17 00:38:09 +0800

    remove unnecessary Grid

--- src/components/SearchUsernameBar.js
+++ src/components/SearchUsernameBar.js
@@ -1,23 +1,19 @@
 import _ from "lodash";
 import React from "react";
-import { Search, Grid } from "semantic-ui-react";
+import { Search } from "semantic-ui-react";
 
 const SearchUsernameBar = props => {
   return (
-    <Grid>
-      <Grid.Column>
-        <Search
-          loading={props.isLoading}
-          onResultSelect={props.handleResultSelect}
-          onSearchChange={_.debounce(props.handleSearchChange, 500, {
-            leading: true
-          })}
-          results={props.results}
-          value={props.value}
-          fluid={true}
-        />
-      </Grid.Column>
-    </Grid>
+    <Search
+      loading={props.isLoading}
+      onResultSelect={props.handleResultSelect}
+      onSearchChange={_.debounce(props.handleSearchChange, 500, {
+        leading: true
+      })}
+      results={props.results}
+      value={props.value}
+      fluid={true}
+    />
   );
 };
 
